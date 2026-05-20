65d703cd0a551e04b897872f2bedfd017a8b8b1a
huzaifan8nexpert-coder <huzaifan8nexpert@gmail.com>
2026-05-20 22:09:38 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# PesePay Backend
+
+Render settings:
+
+Build Command:
+./mvnw clean package -DskipTests
+
+Start Command:
+java -jar target/*.jar
+
+Environment variables:
+PESEPAY_INTEGRATION_KEY
+PESEPAY_ENCRYPTION_KEY
+PESEPAY_RESULT_URL
+PESEPAY_RETURN_URL
+CORS_ALLOWED_ORIGINS
---
+++ src/main/java/com/videogift/pesepay/PesepayBackendApplication.java
@@ -0,0 +1,11 @@
+package com.videogift.pesepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesepayBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PesepayBackendApplication.class, args);
+    }
+}
